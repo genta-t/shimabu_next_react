24e1aa53546f65cffaea1b19967e6cf6c52213ea
田之脇絃太 <gens9@tanowakigenfutoshinoMacBook-Air.local>
2023-03-16 14:03:23 +0900

    Get started by editingに半角スペースの余白

--- components/headline.js
+++ components/headline.js
@@ -6,8 +6,7 @@ export function Headline(props) {
       <h1 className={styles.title}>{props.title}</h1>
 
       <p className={styles.description}>
-        Get started by editing
-        {props.children}
+        Get started by editing {props.children}
       </p>
     </div>
   )
