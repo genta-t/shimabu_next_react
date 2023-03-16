351c098771038b144638d62ca8599d60ed2372ce
田之脇絃太 <gens9@tanowakigenfutoshinoMacBook-Air.local>
2023-03-16 16:23:47 +0900

    useEffectの使い方

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react'
 import { Headline } from '../headline'
 import { Links } from '../links'
 import styles from './Main.module.css'
 
 export function Main(props) {
+  useEffect(() => {
+    // document.body.style.backgroundColor = "lightblue" <-やってはいけない
+    return () => {
+      // document.body.style.backgroundColor = ""　<-やってはいけない
+    }
+  }, [])
+
   return (
     <main className={styles.main}>
       <Headline
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { Footer } from '../components/footer'
 import { Header } from '../components/header'
 import { Main } from '../components/main'
@@ -7,20 +7,22 @@ import styles from '../styles/Home.module.css'
 
 
 export default function Home() {
-  const handleClick = useCallback((e) => {
-    e.preventDefault();
-    alert(123);
-  }, [])
+  // const handleClick = useCallback((e) => {
+  //   e.preventDefault();
+  //   alert(123);
+  // }, [])
   
+
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <button onClick={handleClick}>
+      {/* <button onClick={handleClick}>
         ボタン
-      </button>
+      </button> */}
       <Main title="Index Page" page="index"/>
       <Footer />
     </div>
