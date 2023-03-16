a4469f471c9c0ce1f9bdc93ffc0cb9587644c32e
田之脇絃太 <gens9@tanowakigenfutoshinoMacBook-Air.local>
2023-03-16 15:56:41 +0900

    クリックイベント

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,27 @@
 import Head from 'next/head'
+import { useCallback } from 'react'
 import { Footer } from '../components/footer'
 import { Header } from '../components/header'
 import { Main } from '../components/main'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    e.preventDefault();
+    alert(123);
+  }, [])
+  
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <button onClick={handleClick}>
+        ボタン
+      </button>
       <Main title="Index Page" page="index"/>
-
       <Footer />
     </div>
   )
